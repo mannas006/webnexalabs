299d9d0f10da30e23fda656770b9a5daaf712149
Manas Dey <manasdey.iron006@gmail.com>
2024-09-19 20:50:59 +0530

    fix some icons

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,38 +2,32 @@ const services = [
     {
         title: "Custom Software Development",
         description: "Tailored software solutions designed to streamline your business operations and boost efficiency.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75L15.75 10.5M12 6.75L8.25 10.5M12 6.75V18" /> </svg>`,
     },
     {
         title: "Website Design & Development",
         description: "Modern, responsive websites that captivate your audience and elevate your online presence.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 3h15A1.5 1.5 0 0121 4.5v15A1.5 1.5 0 0119.5 21h-15A1.5 1.5 0 013 19.5v-15A1.5 1.5 0 014.5 3zM3 7.5h18M3 12h18M3 16.5h18" /> </svg>`,
     },
     {
         title: "Mobile App Development",
         description: "Innovative mobile applications that provide seamless experiences across all devices.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M17 2.25h-10A1.5 1.5 0 005.5 3.75v16.5A1.5 1.5 0 007 21h10a1.5 1.5 0 001.5-1.5V3.75A1.5 1.5 0 0017 2.25zM12 18.75h.01M12 15h.01M12 11.25h.01" /> </svg>`,
     },
     {
         title: "SEO & Digital Marketing",
         description: "Strategic digital marketing and SEO services to enhance visibility and drive organic growth.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l7.5 7.5L21.75 3" /> </svg>`,
     },
     {
         title: "Graphics & UI/UX Design",
         description: "Creative design services that bring your brand to life with visually compelling and user-friendly interfaces.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 3L3 6l9 9 3-3 9 9-3 3-6-6 3-3-3-3 3-3L6 3z" /> </svg>`,
     },
     {
         title: "Content Creation & Development",
         description: "High-quality content to engage your audience and communicate your brand’s story effectively.",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 6H19.5A1.5 1.5 0 0121 7.5v9A1.5 1.5 0 0119.5 18H4.5A1.5 1.5 0 013 16.5v-9A1.5 1.5 0 014.5 6zM3 7.5h18M3 12h18m-9 4.5h9" /> </svg>`,
     },
 ];
 
